f1e5a08be163eaec4806911949027020ac30ec7d
kozzztik <kozzztik@mail.ru>
2022-07-12 21:15:12 +0300

    Add protection for proxy server using wrong upstream persistent connections.

--- django_asyncio/middleware.py
+++ django_asyncio/middleware.py
@@ -7,25 +7,36 @@ class SessionMiddleware(DjSessions):
     def process_request(self, request):
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
         context = getattr(request, 'aiohttp_context', None)
+        session = None
         if context:
             session = context.get('session', None)
             if session:
-                request.session = session
-                return
+                if session_key == session.session_key:
+                    request.session = session
+                    return
+                else:
+                    session = None
+                    context['session_miss'] = context.get(
+                        'session_miss', 0) + 1
         request.session = self.SessionStore(session_key)
-        if context:
+        if context and session:
             context['session'] = request.session
 
 
 def get_user(request):
+    session_miss = False
     if not hasattr(request, '_cached_user'):
         context = getattr(request, 'aiohttp_context', {})
         if context:
             user = context.get('user', None)
             if user:
-                request._cached_user = user
-                return request._cached_user
+                session_key = request.sesson.session_key
+                if session_key == context['session'].session_key:
+                    request._cached_user = user
+                    return request._cached_user
+                else:
+                    session_miss = True
         request._cached_user = dj_auth_middleware.auth.get_user(request)
-        if context:
+        if context and not session_miss:
             context['user'] = request._cached_user
     return request._cached_user
